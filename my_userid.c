578723282e63d581027d32636716731a0298c5ec
SnowGoose0 <jason2580134@outlook.com>
2023-05-17 11:26:00 -0700

    fix

--- my_userid.c
+++ my_userid.c
@@ -1,26 +1,49 @@
 #include <stdio.h>
 #include <stdlib.h>
-
-#define STD_INPUT_SIZE        0x1000
-#define STD_CMD_SIZE          0x1040
+#include <string.h>
 
 int main(void) {
-  char username[STD_INPUT_SIZE];
-  char cmd_silent[STD_CMD_SIZE];
-  char cmd[STD_CMD_SIZE];
-  
+
+  FILE* fp;
+  int file_size;
+  char* file_content;
+  char username[300];
+
+  fp = fopen("/etc/passwd", "r");
+  fseek(fp, 0, SEEK_END);
+  file_size = ftell(fp);
+  rewind(fp);
+
+  file_content = (char*) calloc(file_size + 5, sizeof(char));
+  fread(file_content, file_size, sizeof(char), fp);
+  fclose(fp);
+			      
   scanf("%s", username); /* get the username from standard input */
-  sprintf(cmd_silent, "id -u %s > /dev/null 2>&1", username);
-  sprintf(cmd, "id -u %s", username);
 
-  int status = system(cmd_silent); /* silent command to check validity of input */
+  char* tok = strtok(file_content, "\n");
+  while(tok != NULL) {
+    char* tmp = tok;
+    char* u_name = (char*) calloc(300, sizeof(char));
+    
+    for (; *tmp != ':'; ++tmp) u_name[tmp - tok] = *tmp;
 
-  if (status != 0) {
-    printf("no such user found\n");
-    exit(EXIT_FAILURE);
+    if (!strcmp(u_name, username)) {
+      tmp += 3;
+      
+      for (; *tmp != ':'; ++tmp) printf("%c", *tmp);
+      printf("\n");
+      
+      free(file_content);
+      free(u_name);
+      exit(EXIT_SUCCESS);
+    }
+    
+    tok = strtok(NULL, "\n");
+    free(u_name);
   }
 
-  system(cmd); /* display as usual when valid */
+  printf("no such user found");
+  free(file_content);
 
   exit(EXIT_SUCCESS);
 }
